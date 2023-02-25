39f4b3ab5a43660a0eda14834510913907f87936
Jake Potrebic <jake.m.potrebic@gmail.com>
2023-02-25 13:47:13 -0800

    Registry Modification API

--- test-plugin/src/main/java/io/papermc/testplugin/TestPluginBootstrap.java
+++ test-plugin/src/main/java/io/papermc/testplugin/TestPluginBootstrap.java
@@ -2,12 +2,46 @@ package io.papermc.testplugin;
 
 import io.papermc.paper.plugin.bootstrap.PluginBootstrap;
 import io.papermc.paper.plugin.bootstrap.PluginProviderContext;
+import io.papermc.paper.registry.RegistryKey2;
+import io.papermc.paper.registry.RegistryListener;
+import io.papermc.paper.registry.RegistryListenerManager;
+import io.papermc.paper.registry.WritableRegistry;
+import org.bukkit.GameEvent;
+import org.bukkit.NamespacedKey;
 import org.jetbrains.annotations.NotNull;
 
 public class TestPluginBootstrap implements PluginBootstrap {
+
+    static final NamespacedKey NEW_EVENT = new NamespacedKey("machine_maker", "best_event");
+    static final NamespacedKey TO_MODIFY = GameEvent.BLOCK_OPEN.getKey();
 
     @Override
     public void bootstrap(@NotNull PluginProviderContext context) {
     }
 
+    @Override
+    public void registryStuff(RegistryListenerManager manager) {
+        manager.registerListener(RegistryKey2.GAME_EVENT, new Test());
+        manager.registerListener(RegistryKey2.GAME_EVENT, new Test2());
+    }
+
+    private static final class Test implements RegistryListener.Freeze<GameEvent, GameEvent.Builder> {
+
+        @Override
+        public void beforeFreeze(WritableRegistry<GameEvent, GameEvent.Builder> registry) {
+            registry.add(NEW_EVENT, builder -> {
+                builder.range(2);
+            });
+        }
+    }
+
+    private static final class Test2 implements RegistryListener.Modification<GameEvent.Builder> {
+
+        @Override
+        public void onRegister(GameEvent.Builder builder) {
+            if (builder.key().equals(TO_MODIFY)) {
+                builder.range(builder.range() * 2);
+            }
+        }
+    }
 }
