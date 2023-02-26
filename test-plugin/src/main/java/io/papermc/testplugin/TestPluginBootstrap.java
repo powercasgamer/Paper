9025d2862a937a49441a315ef91cedffe18c529f
Jake Potrebic <jake.m.potrebic@gmail.com>
2023-02-26 14:42:27 -0800

    beef up event system

--- test-plugin/src/main/java/io/papermc/testplugin/TestPluginBootstrap.java
+++ test-plugin/src/main/java/io/papermc/testplugin/TestPluginBootstrap.java
@@ -6,12 +6,12 @@ import io.papermc.paper.plugin.bootstrap.PluginProviderContext;
 import io.papermc.paper.registry.RegistryKey2;
 import io.papermc.paper.registry.RegistryListener;
 import io.papermc.paper.registry.RegistryListenerManager;
-import io.papermc.paper.registry.WritableRegistry;
+import io.papermc.paper.registry.event.FreezeRegistryEvent;
+import io.papermc.paper.registry.event.ModifyRegistryEvent;
 import org.bukkit.GameEvent;
 import org.bukkit.NamespacedKey;
 import org.checkerframework.checker.nullness.qual.NonNull;
 import org.checkerframework.framework.qual.DefaultQualifier;
-import org.jetbrains.annotations.NotNull;
 
 @DefaultQualifier(NonNull.class)
 public class TestPluginBootstrap implements PluginBootstrap {
@@ -33,41 +33,37 @@ public class TestPluginBootstrap implements PluginBootstrap {
     }
 
     private static final class Test implements RegistryListener.Freeze<GameEvent, GameEvent.Builder> {
-
         @Override
-        public void beforeFreeze(WritableRegistry<GameEvent, GameEvent.Builder> registry) {
-            registry.register(NEW_EVENT, builder -> {
+        public void beforeFreeze(final FreezeRegistryEvent<GameEvent, GameEvent.Builder> event) {
+            event.registryView().register(NEW_EVENT, builder -> {
                 builder.range(2);
             });
         }
     }
 
     private static final class Test2 implements RegistryListener.Modification<GameEvent, GameEvent.Builder> {
-
         @Override
-        public void onRegister(GameEvent.Builder builder) {
-            if (builder.key().equals(TO_MODIFY)) {
-                builder.range(builder.range() * 2);
+        public void beforeRegister(final ModifyRegistryEvent<GameEvent, GameEvent.Builder> event) {
+            if (event.currentBuilder().key().equals(TO_MODIFY)) {
+                event.currentBuilder().range(event.currentBuilder().range() * 2);
             }
         }
     }
 
     private static final class AddChatType implements RegistryListener.Freeze<ChatType, ChatType.Builder> {
-
         @Override
-        public void beforeFreeze(final WritableRegistry<ChatType, ChatType.Builder> registry) {
-            registry.register(NEW_CHAT, builder -> {
+        public void beforeFreeze(final FreezeRegistryEvent<ChatType, ChatType.Builder> event) {
+            event.registryView().register(NEW_CHAT, builder -> {
                 builder.textTranslationKey("epic chat from %s: %s");
             });
         }
     }
 
     private static final class ModifyChatType implements RegistryListener.Modification<ChatType, ChatType.Builder> {
-
         @Override
-        public void onRegister(final ChatType.@NotNull Builder builder) {
-            if (builder.key().equals(net.kyori.adventure.chat.ChatType.CHAT.key())) {
-                builder.textTranslationKey("<<%s>> %s");
+        public void beforeRegister(final ModifyRegistryEvent<ChatType, ChatType.Builder> event) {
+            if (event.currentBuilder().key().equals(net.kyori.adventure.chat.ChatType.CHAT.key())) {
+                event.currentBuilder().textTranslationKey("<<%s>> %s");
             }
         }
     }
